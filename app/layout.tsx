ffa9fa09e948356958b0177a885bf260444052e2
GenerousPharaoh <kareemhassanein@gmail.com>
2026-03-26 00:30:03 -0400

    Optimize font loading path

--- app/layout.tsx
+++ app/layout.tsx
@@ -409,6 +409,10 @@ export default function RootLayout({
         {/* Preconnect to Google Fonts for faster loading */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap"
+        />
       </head>
       <body>
         {/* Google Analytics */}
